cbfb11c0cbdb4e1881e93bff6a7f4c776c5a16ee
YunGyeongHyeon <y300513@gmail.com>
2020-02-05 17:56:26 +0900

    2020.02.05

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # WeTube
 
 Cloning Youtube with Vanilla and NodeJS
+
+## Pages:
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Video Detail
+- [ ] Edit Video
---
+++ controller/userController.js
@@ -0,0 +1,39 @@
+import routes from '../routes'
+export const getJoin = (req, res) => {
+  res.render('join', { pageTitle: 'Join' })
+}
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req
+  if (password !== password2) {
+    res.status(400)
+    res.render('join', { pageTitle: 'Join' })
+  } else {
+    //To Do : register User
+    //To Do :
+    res.redirect(routes.home)
+  }
+  res.render('join', { pageTitle: 'Join' })
+}
+
+export const getLogin = (req, res) =>
+  res.render('login', { pageTitle: 'Login' })
+
+export const postLogin = (req, res) => {
+  res.redirect(routes.home)
+}
+
+export const logout = (req, res) => {
+  // To Do: Process Log Out
+  res.redirect(routes.home)
+}
+export const userDetail = (req, res) =>
+  res.render('userDetail', { pageTitle: 'UserDetail' })
+
+export const editProfile = (req, res) =>
+  res.render('editProfile', { pageTitle: 'EditProfile' })
+
+export const changePassword = (req, res) =>
+  res.render('changePassword', { pageTitle: 'ChangePassword' })
