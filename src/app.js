9b90bba7c26ab6b20c4cec26342a1e07b02e1dd0
YunGyeongHyeon <y300513@gmail.com>
2020-03-06 14:09:20 +0900

    ooo

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import videoRouter from './routers/videoRouter'
 import globalRouter from './routers/globalRouter'
 import apiRouter from './routers/apiRouter'
 import routes from './routes'
+
 import './passport'
 
 const app = express()
